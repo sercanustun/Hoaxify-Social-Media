a28fef1621dd6e3cfe9deb21d5f6b7db352deee6
sercan <ustunsercan@gmail.com>
2022-04-28 04:12:29 +0300

    feat: botstrap added and overrided

--- frontend/src/UserSignupPage.js
+++ frontend/src/UserSignupPage.js
@@ -31,30 +31,50 @@ class UserSignupPage extends React.Component {
 
   render() {
     return (
-      <form>
-        <h1>Sign Up</h1>
-        <div>
-          <label>username</label>
-          <input name="username" onChange={this.onChange} />
-        </div>
-        <div>
-          <label>Display Name</label>
-          <input name="displayName" onChange={this.onChange} />
-        </div>
-        <div>
-          <label>Password</label>
-          <input name="password" onChange={this.onChange} type={"password"} />
-        </div>
-        <div>
-          <label>Password Repeat</label>
-          <input
-            name="passwordRepeat"
-            onChange={this.onChange}
-            type={"password"}
-          />
-        </div>
-        <button onClick={this.onClickedSignup}>Sign Up</button>
-      </form>
+      <div className="container">
+        <form>
+          <h1 className="text-center">Sign Up</h1>
+          <div className="form-group">
+            <label>username</label>
+            <input
+              className="form-control"
+              name="username"
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Display Name</label>
+            <input
+              className="form-control"
+              name="displayName"
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              className="form-control"
+              name="password"
+              onChange={this.onChange}
+              type={"password"}
+            />
+          </div>
+          <div className="form-group">
+            <label>Password Repeat</label>
+            <input
+              name="passwordRepeat"
+              onChange={this.onChange}
+              type={"password"}
+              className="form-control"
+            />
+          </div>
+          <div className="text-center">
+            <button className="btn btn-primary" onClick={this.onClickedSignup}>
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
